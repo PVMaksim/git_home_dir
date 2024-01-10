7adca5e9d408595b0a8879f2f6ee9776e218eb30
Maksim Vladimirovich <m008ba@mail.ru>
2024-01-10 16:24:41 +0300

    еще одна версия paskal + создал новую ветку paskal

--- task/4.3.10_paskal.py
+++ task/4.3.10_paskal.py
@@ -1,22 +1,16 @@
 n = int(input())
-k = [1, 1]  # соответствует 2 строке (n=2)
-new_k = [1, 1]
 
 
-def paskal(n):  # создает следующую строку пирамиды
-    if n == 0:
-        k = [1]
-    elif n == 1:
-        k = [1, 1]
-    else:
-        k = [1, 1]
-        new_k = [1, 1]
-        while len(k) <= n:
-            for i in range(len(k)-1):
-                new_k.insert(i + 1, k[i]+k[i + 1])
-            k = [1, 1]
-            k, new_k = new_k, k
-    return k
+def paskal(n):
+    new_k = []
+    k = [1]
+    for _ in range(n-1):
+        k = [0] + k + [0]
+        for i in range(len(k)-1):
+            new_k.append((k[i] + k[i+1]))
+        print(*new_k)
+        new_k, k = [], new_k
 
 
-print(paskal(n))
+print(1)
+paskal(n)
