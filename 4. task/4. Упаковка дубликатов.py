f1c6b008c655b1461491905dc0b71d30b14d22bb
Maksim Vladimirovich <m008ba@mail.ru>
2024-01-11 16:54:19 +0300

    доделал прогу по дубликатам

--- 4. task/4. Упаковка дубликатов.py
+++ 4. task/4. Упаковка дубликатов.py
@@ -1,4 +1,12 @@
-s = 'a b b b c d'
-lst = s.split()
 
-rez = []
+lst = input().split()
+
+rez = [lst.pop(0)]
+print(rez, lst)
+for i in range(len(lst)):
+    char = lst.pop(0)
+    if char not in rez[-1]:
+        rez += [list(char)]
+    else:
+        rez[-1] += [char]
+print(rez)
