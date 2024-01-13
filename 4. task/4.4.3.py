759cdcc9ac069b6c9bacba0a759f4d73b925c04c
Maksim Vladimirovich <m008ba@mail.ru>
2024-01-13 12:42:58 +0300

    внес изменени

--- 4. task/4.4.3.py
+++ 4. task/4.4.3.py
@@ -1,13 +1,1 @@
 n = 2
-matrix = []
-mx = 0
-for _ in range(n):
-    tamp = [int(i) for i in input().split()]
-    matrix.append(tamp)
-
-for r in range(n):
-    for c in range(n):
-        if r <= c:
-            if matrix[r][c] > mx:
-                mx = matrix[r][c]
-print(mx)
