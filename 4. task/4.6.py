c2d8d8e51a3268f0ab1a615ece5b79d53174c642
Maksim Vladimirovich <m008ba@mail.ru>
2024-01-19 13:03:27 +0300

    решение работает тольно для квадратной матрици. если рядов больше и они четный то все ломается

--- 4. task/4.6.py
+++ 4. task/4.6.py
@@ -2,21 +2,41 @@
 n = 4
 m = 5
 matrix = [[0] * m for i in range(n)]
+spiral = 0
 cnt = 0
-for k in range(1):
-    for i in range(n):
-        for j in range(m):
-            if i == k:
-                matrix[i][j] = cnt+1
-            cnt += 1
-            # elif j == m-1:
-            #     # c += 1
-            #     matrix[i][j] = cnt+1
-            #     cnt += 1
-            # elif
 
+for i in range(m):
+    cnt += 1
+    matrix[0][i] = cnt
+i = 0
+j = m-1
 
-for i in range(n):
-    for j in range(m):
+while cnt <= len(matrix)*len(matrix[0]):
+
+    for c in range(n-1):  # вниз
+        i += 1
+        cnt += 1
+        matrix[i][j] = cnt
+
+    for c in range(m-1):  # влево
+        cnt += 1
+        j -= 1
+        matrix[i][j] = cnt
+
+    for c in range(n-1-1):  # вверх
+        i -= 1
+        cnt += 1
+        matrix[i][j] = cnt
+
+    for c in range(m-1-1):
+        j += 1
+        cnt += 1
+        matrix[i][j] = cnt
+
+    n -= 2
+    m -= 2
+
+for i in range(len(matrix)):
+    for j in range(len(matrix[0])):
         print(str(matrix[i][j]).ljust(3), end=' ')
     print()
