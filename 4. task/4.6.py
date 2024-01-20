2c8fe9c4123c3d31fcc1beec3706c1fb2aa1af92
Maksim Vladimirovich <m008ba@mail.ru>
2024-01-20 19:38:39 +0300

    решение заполнения матрици по спирали

--- 4. task/4.6.py
+++ 4. task/4.6.py
@@ -1,40 +1,34 @@
 # n, m = [int(i) for i in input().split()]
 n = 4
-m = 5
+m = 8
 matrix = [[0] * m for i in range(n)]
-spiral = 0
-cnt = 0
 
-for i in range(m):
-    cnt += 1
-    matrix[0][i] = cnt
+cnt = 1
+
 i = 0
-j = m-1
+j = -1
 
-while cnt <= len(matrix)*len(matrix[0]):
-
-    for c in range(n-1):  # вниз
-        i += 1
-        cnt += 1
+d_row = 0
+d_cols = 1
+while cnt != m*n+1:
+    if 0 <= i+d_row <= n-1 and 0 <= j+d_cols <= m-1 and matrix[i+d_row][j+d_cols] == 0:
+        i = i + d_row
+        j = j + d_cols
         matrix[i][j] = cnt
-
-    for c in range(m-1):  # влево
         cnt += 1
-        j -= 1
-        matrix[i][j] = cnt
-
-    for c in range(n-1-1):  # вверх
-        i -= 1
-        cnt += 1
-        matrix[i][j] = cnt
-
-    for c in range(m-1-1):
-        j += 1
-        cnt += 1
-        matrix[i][j] = cnt
-
-    n -= 2
-    m -= 2
+    else:
+        if d_cols == 1:  # если шли вправо
+            d_cols = 0
+            d_row = 1
+        elif d_row == 1:  # если шли вниз
+            d_row = 0
+            d_cols = -1
+        elif d_cols == -1:  # если шли влево
+            d_cols = 0
+            d_row = -1
+        elif d_row == -1:  # если шли вверх
+            d_row = 0
+            d_cols = 1
 
 for i in range(len(matrix)):
     for j in range(len(matrix[0])):
