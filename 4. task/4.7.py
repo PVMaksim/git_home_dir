bc76cf7a76fcd8c0ae719bc419e51a5f2f090451
Maksim Vladimirovich <m008ba@mail.ru>
2024-01-21 15:41:54 +0300

    программа по перемножению матриц сделана

--- 4. task/4.7.py
+++ 4. task/4.7.py
@@ -1,19 +1,29 @@
 # n, m = [int(i) for i in input().split()]
-n = 4
-m = 1
+n = 3
+m = 2
 
-mas_a = [[int(j) for j in (input().split())] for _ in range(n)]
+# mas_a = [[int(j) for j in (input().split())] for _ in range(n)]
+mas_a = [[2, 5],
+         [6, 7],
+         [1, 8]]
+
 print()
-mas_b = [[int(j) for j in (input().split())] for _ in range(n)]
+# m, k = [int(i) for i in input().split()]
+m = 2
+k = 3
 
+# mas_b = [[int(j) for j in (input().split())] for _ in range(n)]
+mas_b = [[1, 2, 1],
+         [0, 1, 0]]
 
-mas_c = [[0]*m for _ in range(n)]
-# mas_c =
+mas_c = [[0]*k for _ in range(n)]
+
 for i in range(n):
-    for j in range(m):
-        mas_c[i][j] = mas_a[i][j] + mas_b[i][j]
+    for j in range(k):
+        for x in range(m):
+            mas_c[i][j] += mas_a[i][x] * mas_b[x][j]
 
 for i in range(n):
-    for j in range(m):
+    for j in range(k):
         print(str(mas_c[i][j]).ljust(3), end=' ')
     print()
